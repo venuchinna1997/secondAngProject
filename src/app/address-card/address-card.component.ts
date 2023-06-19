b3e2b079512fa502bebd350247d640dc58b96c01
vgrgala@gmail.com <Chinna@1025>
2023-06-18 19:01:04 -0500

    passing inputs to components

--- src/app/address-card/address-card.component.ts
+++ src/app/address-card/address-card.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-address-card',
   templateUrl: './address-card.component.html',
   styleUrls: ['./address-card.component.css']
 })
-export class AddressCardComponent {
+export class AddressCardComponent implements OnInit{
 
   user : any;
+  @Input("name") userTitle : any
 
   constructor() {
+  }
+  ngOnInit(): void {
     this.user = {
-      name: 'VGR',
+      name: this.userTitle,
       title: 'Software Developer',
       address: '336 Firwood Dr, Apt B, Dayton, Ohio, 45419',
       phone:[
